997a37d574be6a66de269a1e125c07037249d3f1
Alison Tahiri <alison.tahiri@gmail.com>
2023-07-18 14:17:16 +0200

    (fix): add api transfer salt env

--- config/admin.js
+++ config/admin.js
@@ -1,9 +1,15 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-
-});
+module.exports = ({ env }) => {
+  return {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+    apiToken: {
+      salt: env("API_TOKEN_SALT"),
+    },
+    transfer: {
+      token: {
+        salt: env("API_TRANSFER_SALT"),
+      },
+    },
+  };
+};
